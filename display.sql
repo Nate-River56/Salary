52c9c0350920e2fabc03e7e6142f7241b6e6aaab
Nate-River56 <nate@neito-MacBook.local>
2016-08-10 03:27:34 +0900

    いい感じに結果を出すSQLファイルを配置

---
+++ display.sql
@@ -0,0 +1,23 @@
+SELECT 
+  cpn.business      AS '業種',
+  cpn.name          AS '会社名',
+  member            AS '従業員数',
+  ave_age           AS '平均年齢',
+  ave_annual_income AS '平均年収(万円)'
+FROM
+  salary_2016
+INNER JOIN
+(
+  SELECT 
+    company.id    AS id,
+    company.name  AS name,
+    business.name AS business
+  FROM
+    company
+  INNER JOIN
+    business
+  ON
+    company.business_id = business.id
+) cpn
+ON
+  salary_2016.company_id = cpn.id;

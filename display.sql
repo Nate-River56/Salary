522ff379504afbf3f4bdb1ec199ad4c5ac4fff0b
Nate-River56 <nate@neito-MacBook.local>
2016-08-10 04:34:53 +0900

    SQLの変更, スキーマの変更

--- display.sql
+++ display.sql
@@ -3,9 +3,10 @@ SELECT
   cpn.name          AS '会社名',
   member            AS '従業員数',
   ave_age           AS '平均年齢',
-  ave_annual_income AS '平均年収(万円)'
+  ave_annual_income AS '平均年収(万円)',
+  year              AS '年度'
 FROM
-  salary_2016
+  salary
 INNER JOIN
 (
   SELECT 
@@ -20,4 +21,7 @@ INNER JOIN
     company.business_id = business.id
 ) cpn
 ON
-  salary_2016.company_id = cpn.id;
+  salary.company_id = cpn.id
+WHERE
+  year == 2016
+;

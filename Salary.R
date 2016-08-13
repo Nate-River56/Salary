fa9e2f7afb1bb1f319519acb0c3af7468b2b020b
cy-kosuke-ono <kosuke_ono@cybird.co.jp>
2016-08-14 02:51:14 +0900

    R6Classの追加
    R6Classを用いた表現に変更した
    。

--- Salary.R
+++ Salary.R
@@ -1,32 +1,145 @@
 #! /usr/bin/env Rscript
-require("RSQLite")
-require("extrafont")
-require("Cairo")
 
-par(family="HiraginoSans-W3")
-par(xpd=TRUE)
+require("R6")
 
-CairoFonts(regular = "HiraginoSans-W3", bold = "HiraginoSans-W5")
+# R6 class
+Salary<-R6Class(
+  classname = "Salary",
+  private = list(
+    # Private Variables
+    dbengine = "SQLite",
+    sqlname = "display.sql",
+    dbname = "salary.sqlite",
+    pdfname = "salary_analyze.pdf",
+    fontreg = "HiraginoSans-W3",
+    fontbol = "HiraginoSans-W5",
+    con = NA,
 
-con<-dbConnect(dbDriver("SQLite"), dbname="salary.sqlite")
+    # Private Methods
+    loadModules = function()
+    {
+      require("RSQLite")
+      require("extrafont")
+      require("Cairo")
+      require("ggplot2")
+    },
+    gpTheme = function(gp, title.text)
+    {
+      gp = gp + geom_smooth(method=glm, family=gaussian)
+      gp = gp + labs(title=title.text)
+      gp = gp + theme_bw()
+      gp = gp + theme(panel.grid.minor=element_blank())
+      return(gp)
+    },
+    db.open = function()
+    {
+      private$con<-
+        dbConnect(
+          dbDriver(private$dbengine),
+          dbname = private$dbname
+        )
+    },
+    getQuery = function()
+    {
+      self$query<-readChar(
+        private$sqlname,
+        file.info(private$sqlname)$size
+      )
+      salary.raw<-dbGetQuery(
+        conn = private$con,
+        statement = self$query
+      )
+      self$salary.data<-salary.raw[,3:5]
+      row.names(self$salary.data)<-salary.raw[,2]
+    },
+    db.close = function()
+    {
+      dbDisconnect(private$con)
+    },
+    data.load = function()
+    {
+      private$db.open()
+      private$getQuery()
+      private$db.close()
+    },
+    config.font = function()
+    {
+      par(family=private$fontreg)
+      par(xpd=TRUE)
+      CairoFonts(
+        regular = private$fontreg,
+        bold = private$fontbol
+      )
+    }
+  ),
+  public = list(
+    # Public Variables
+    query = NA,
+    salary.data = NA,
+    prcomp.result = NA,
+    hclust.result = NA,
+    
+    # Constructor Method
+    initialize = function()
+    {
+      private$loadModules()
+      private$data.load()
+      private$config.font()
+    },
+    
+    # Public Methods
+    PDF.open = function(filename=private$pdfname,
+                        pdf.height = 9.27,
+                        pdf.width = 30
+                        )
+    {
+      CairoPDF(
+        file = filename,
+        onefile = TRUE,
+        paper="a4r",
+        height = pdf.height,
+        width = pdf.width)
+    },
+    PDF.close = function()
+    {
+      dev.off()
+    },
+    draw.biplot = function()
+    {
+      self$prcomp.result<-prcomp(self$salary.data, scale=TRUE)
+      biplot(self$prcomp.result)
+    },
+    draw.pairs = function()
+    {
+      pairs(self$salary.data, pch=16, col="blue")
+    },
+    draw.dendrogram = function(cluster.method="ward.D2")
+    {
+      self$hclust.result<-
+        hclust(
+          dist(self$salary.data),
+          method = cluster.method
+        )
+      summary(self$hclust.result)
+      plot(
+        self$hclust.result,
+        hang=-1,
+        sub = paste("Clustering method: ",cluster.method)
+      )
+    },
+    cut.tree = function(k)
+    {
+      result<-cutree(self$hclust.result)
+      print(result)
+      self$salary["group.estinate"]<-result
+    },
+    query.print = function()
+    {
+      cat(self$query)
+    }
+  )
+)
 
-filename<-"display.sql"
-query<-readChar(filename,file.info(filename)$size)
-salary<-dbGetQuery(con,statement = query)
-
-dbDisconnect(con)
-
-salary2<-salary[,3:5]
-row.names(salary2)<-salary[,2]
-
-result<-prcomp(salary2, scale=TRUE)
-
-CairoPDF(file = "salary_analyze.pdf", onefile = TRUE, paper="a4r", height = 9.27, width = 30)
-pairs(salary2, pch=16, col="blue")
-biplot(result)
-plot(hclust(dist(salary2)),hang=-1)
-
-dev.off()
 
 
 
